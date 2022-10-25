a565475103155425688f85746462f5e7f882c333
CieloChiara <cielo.chiara701@gmail.com>
2022-10-26 05:37:56 +0900

    Add mint_token_id page

---
+++ pages/mint_token_id.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+//import Header from '@components/Header'
+
+const Canvas = dynamic(() => import('../components/mintTokenIdCanvas'), {
+  ssr: false,
+  loading: () => <p>Now Loading...</p>,
+})
+
+const MintPage: NextPage = () => {
+  return (
+    <>
+      <main>
+        <Canvas />
+      </main>
+    </>
+  )
+};
+
+export default MintPage;
